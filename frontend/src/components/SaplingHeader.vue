f3e013ae6fc2219394d604aca0fe7d9ee88d0a24
martin-rosbund <martin.rosbund@gmail.com>
2025-10-15 22:10:56 +0200

    add inbox

--- frontend/src/components/SaplingHeader.vue
+++ frontend/src/components/SaplingHeader.vue
@@ -1,44 +1,54 @@
 <template>
-    <v-app-bar :elevation="2">
-      <template v-slot:prepend>
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      </template>
+  <v-app-bar :elevation="2">
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    </template>
 
-      <v-app-bar-title>
-        <v-btn stacked @click="$router.push('/')">Startseite</v-btn>
-      </v-app-bar-title>
+    <v-app-bar-title>
+      <v-btn stacked @click="$router.push('/')">Startseite</v-btn>
+    </v-app-bar-title>
 
-      <template v-slot:append>
-        <span style="margin-left: 16px; font-weight: normal;">{{ time }}</span>
-        <v-btn class="text-none" stacked>
-          <v-badge location="top right" color="primary" content="5">
-            <v-icon icon="mdi-email"></v-icon>
-          </v-badge>
-        </v-btn>
-        <v-btn stacked @click="$router.push('/account')">
-            <v-icon icon="mdi-account"></v-icon>
-        </v-btn>
-      </template>
-    </v-app-bar>
+    <template v-slot:append>
+      <span style="margin-left: 16px; font-weight: normal;">{{ time }}</span>
+      <v-btn class="text-none" stacked @click="showInbox = true">
+        <v-badge location="top right" color="primary" content="5">
+          <v-icon icon="mdi-email"></v-icon>
+        </v-badge>
+      </v-btn>
+      <v-btn stacked @click="$router.push('/account')">
+        <v-icon icon="mdi-account"></v-icon>
+      </v-btn>
+    </template>
+  </v-app-bar>
 
   <SaplingNavigation v-model="drawer" />
+
+  <!-- Inbox Modal -->
+  <v-dialog v-model="showInbox" max-width="90vw" max-height="90vh" scrollable>
+    <div style="max-height:80vh; overflow-y:auto;">
+      <Inbox />
+    </div>
+  </v-dialog>
 </template>
 
-<script lang="ts" setup>
-  import { ref, onMounted, onUnmounted } from 'vue'
-  import SaplingNavigation from './SaplingNavigation.vue'
-  const drawer = ref(false)
+<script lang="ts" setup> 
+import { ref, onMounted, onUnmounted } from 'vue'
+import SaplingNavigation from './SaplingNavigation.vue'
+import Inbox from './Inbox.vue' // Importiere deine Inbox-Komponente
 
-  const time = ref(new Date().toLocaleTimeString())
-  let timer: number
+const drawer = ref(false)
+const showInbox = ref(false) // Dialog-Status
 
-  onMounted(() => {
-    timer = window.setInterval(() => {
-      time.value = new Date().toLocaleTimeString()
-    }, 1000)
-  })
+const time = ref(new Date().toLocaleTimeString())
+let timer: number
 
-  onUnmounted(() => {
-    clearInterval(timer)
-  })
+onMounted(() => {
+  timer = window.setInterval(() => {
+    time.value = new Date().toLocaleTimeString()
+  }, 1000)
+})
+
+onUnmounted(() => {
+  clearInterval(timer)
+})
 </script>
