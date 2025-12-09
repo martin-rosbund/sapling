f4d9a79a5c4dec487fe3cfd5ca0fcdfeb7ad0961
martin-rosbund <martin.rosbund@gmail.com>
2025-12-09 21:54:28 +0100

    right management templates

--- backend/src/database/seeder/PermissionSeeder.ts
+++ backend/src/database/seeder/PermissionSeeder.ts
@@ -11,21 +11,57 @@ export class PermissionSeeder extends Seeder {
    */
   async run(em: EntityManager): Promise<void> {
     const count = await em.count(PermissionItem);
-    const entities = await em.findAll(EntityItem);
+    const entities = await em.findAll(EntityItem, { populate : ['group'] });
     const roles = await em.findAll(RoleItem);
 
     if (count === 0) {
       for (const entity of entities) {
         for (const role of roles) {
-          em.create(PermissionItem, {
-            allowRead: true,
-            allowInsert: true,
-            allowUpdate: true,
-            allowDelete: true,
-            allowShow: true,
-            entity: entity,
-            role: role,
-          });
+          switch(role.handle ?? 0){
+            case 1:
+            em.create(PermissionItem, {
+              allowRead: true,
+              allowInsert: true,
+              allowUpdate: true,
+              allowDelete: true,
+              allowShow: true,
+              entity: entity,
+              role: role,
+            });
+              break;
+            case 2:
+              em.create(PermissionItem, {
+                allowRead: !entity.canRead || ['masterdata', 'processing'].includes(entity.group?.handle ?? ''),
+                allowInsert: ['masterdata', 'processing'].includes(entity.group?.handle ?? ''),
+                allowUpdate: ['masterdata', 'processing'].includes(entity.group?.handle ?? ''),
+                allowDelete: ['masterdata', 'processing'].includes(entity.group?.handle ?? ''),
+                allowShow: ['masterdata', 'processing'].includes(entity.group?.handle ?? ''),
+                entity: entity,
+                role: role,
+              });
+              break;
+            case 3:
+              em.create(PermissionItem, {
+                allowRead: !entity.canRead || ['masterdata', 'processing'].includes(entity.group?.handle ?? ''),
+                allowInsert: false,
+                allowUpdate: false,
+                allowDelete: false,
+                allowShow: ['masterdata', 'processing'].includes(entity.group?.handle ?? ''),
+                entity: entity,
+                role: role,
+              });
+              break;
+            default:
+              em.create(PermissionItem, {
+                allowRead: !entity.canRead || ['masterdata', 'processing'].includes(entity.group?.handle ?? ''),
+                allowInsert: false,
+                allowUpdate: false,
+                allowDelete: false,
+                allowShow: ['masterdata', 'processing'].includes(entity.group?.handle ?? ''),
+                entity: entity,
+                role: role,
+              });
+          }
         }
       }
     }
