34e953a4bf84a4c43ae0592cd4c7b5a94e14d017
Martin Rosbund <mrosbund@isb-solutions.de>
2025-12-16 12:13:25 +0100

    remove useless css

--- frontend/src/components/dashboard/SaplingDashboard.vue
+++ frontend/src/components/dashboard/SaplingDashboard.vue
@@ -5,50 +5,53 @@
       class="fill-height glass-panel"
       type="paragraph"/>
     <template v-else>
-      <v-container class="fill-height pa-0" fluid>
-          <!-- Tabs for user-configurable dashboards -->
-          <div class="sapling-dashboard-tabs-wrapper d-flex align-center" style="overflow-x: auto; white-space: nowrap; max-width: 100vw;">
-            <VTabs
-              v-model="activeTab"
-              background-color="primary"
-              dark
-              height="44"
-              class="sapling-dashboard-tabs"
-              show-arrows
-              style="min-width: 0; width: 100%; max-width: 100vw;"
-            >
-              <VTab v-for="(tab, idx) in userTabs" :key="tab.id" style="min-width: 120px;">
-                <div class="d-flex align-center sapling-dashboard-tab">
-                  <v-icon class="mr-1" v-if="tab.icon">{{ tab.icon }}</v-icon>
-                  <span class="mr-2">{{ tab.title }}</span>
-                  <v-btn-group>
-                    <v-btn icon="mdi-close" size="x-small" class="transparent" @click.stop="removeTab(idx)" v-if="userTabs.length > 1"/>
-                  </v-btn-group>
-                </div>
-              </VTab>
-              <VTab @click.stop="openDashboardDialog" class="d-flex align-center sapling-dashboard-tab-add" style="min-width: 80px;">
-                <v-icon>mdi-plus</v-icon>
-              </VTab>
-              <!-- Dashboard Anlage Dialog -->
-              <SaplingEdit
-                v-model="dashboardDialog"
-                :mode="'create'"
-                :item="null"
-                :templates="dashboardTemplates || []"
-                :entity="dashboardEntity"
-                @save="onDashboardSave"
-                @cancel="dashboardDialog = false"
-              />
-            </VTabs>
-            <v-btn-group>
-              <v-btn
-                icon="mdi-star-outline"
-                class="transparent"
-                @click="favoritesDrawer = !favoritesDrawer"
-                :title="$t('navigation.favorite')">
-              </v-btn>
-            </v-btn-group>
-          </div>
+      <v-container class=" pa-0" fluid>
+        <v-row class="fill-height" no-gutters>
+          <v-col cols="12" class="d-flex flex-column">
+            <div class="d-flex align-center">
+              <VTabs
+                v-model="activeTab"
+                background-color="primary"
+                dark
+                height="44"
+                class="sapling-dashboard-tabs"
+                show-arrows
+                style="min-width: 0; width: 100%; max-width: 100vw;"
+              >
+                <VTab v-for="(tab, idx) in userTabs" :key="tab.id" style="min-width: 120px;">
+                  <div class="d-flex align-center sapling-dashboard-tab">
+                    <v-icon class="mr-1" v-if="tab.icon">{{ tab.icon }}</v-icon>
+                    <span class="mr-2">{{ tab.title }}</span>
+                    <v-btn-group>
+                      <v-btn icon="mdi-close" size="x-small" class="transparent" @click.stop="removeTab(idx)" v-if="userTabs.length > 1"/>
+                    </v-btn-group>
+                  </div>
+                </VTab>
+                <VTab @click.stop="openDashboardDialog" class="d-flex align-center sapling-dashboard-tab-add" style="min-width: 80px;">
+                  <v-icon>mdi-plus</v-icon>
+                </VTab>
+                <!-- Dashboard Anlage Dialog -->
+                <SaplingEdit
+                  v-model="dashboardDialog"
+                  :mode="'create'"
+                  :item="null"
+                  :templates="dashboardTemplates || []"
+                  :entity="dashboardEntity"
+                  @save="onDashboardSave"
+                  @cancel="dashboardDialog = false"
+                />
+              </VTabs>
+              <v-btn-group>
+                <v-btn
+                  icon="mdi-star-outline"
+                  class="transparent"
+                  @click="favoritesDrawer = !favoritesDrawer"
+                  :title="$t('navigation.favorite')">
+                </v-btn>
+              </v-btn-group>
+            </div>
+          </v-col>
+        </v-row>
         <VWindow v-model="activeTab">
           <VWindowItem v-for="(tab, idx) in userTabs" :key="tab.id" :value="idx">
             <DashboardKpis
@@ -57,7 +60,6 @@
               :activeTab="activeTab"/>
           </VWindowItem>
         </VWindow>
-
         <SaplingDelete
           v-model:modelValue="dashboardDeleteDialog"
           :item="dashboardToDelete"
