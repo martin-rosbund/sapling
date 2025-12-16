c099ccf8bd78217b98a8e4f31bb7fc63a7d180a3
Martin Rosbund <mrosbund@isb-solutions.de>
2025-12-16 11:04:33 +0100

    favories for small displays

--- frontend/src/components/dashboard/SaplingDashboard.vue
+++ frontend/src/components/dashboard/SaplingDashboard.vue
@@ -6,64 +6,67 @@
       type="paragraph"/>
     <template v-else>
       <v-container class="fill-height pa-0" fluid>
-        <v-row class="fill-height" no-gutters>
-          <!-- Main Dashboard Area -->
-            <v-col cols="12" md="10" class="d-flex flex-column sapling-dashboard-main" style="min-width: 0;">
-              <!-- Tabs for user-configurable dashboards -->
-              <div class="sapling-dashboard-tabs-wrapper" style="overflow-x: auto; white-space: nowrap; max-width: 100vw;">
-                <VTabs
-                  v-model="activeTab"
-                  background-color="primary"
-                  dark
-                  height="44"
-                  class="sapling-dashboard-tabs"
-                  show-arrows
-                  style="min-width: 0; width: 100%; max-width: 100vw;"
-                >
-                  <VTab v-for="(tab, idx) in userTabs" :key="tab.id" style="min-width: 120px;">
-                    <div class="d-flex align-center sapling-dashboard-tab">
-                      <v-icon class="mr-1" v-if="tab.icon">{{ tab.icon }}</v-icon>
-                      <span class="mr-2">{{ tab.title }}</span>
-                      <v-btn-group>
-                        <v-btn icon="mdi-close" size="x-small" class="transparent" @click.stop="removeTab(idx)" v-if="userTabs.length > 1"/>
-                      </v-btn-group>
-                    </div>
-                  </VTab>
-                  <VTab @click.stop="openDashboardDialog" class="d-flex align-center sapling-dashboard-tab-add" style="min-width: 80px;">
-                    <v-icon>mdi-plus</v-icon>
-                  </VTab>
-                  <!-- Dashboard Anlage Dialog -->
-                  <SaplingEdit
-                    v-model="dashboardDialog"
-                    :mode="'create'"
-                    :item="null"
-                    :templates="dashboardTemplates || []"
-                    :entity="dashboardEntity"
-                    @save="onDashboardSave"
-                    @cancel="dashboardDialog = false"
-                  />
-                </VTabs>
-              </div>
-            <VWindow v-model="activeTab">
-              <VWindowItem v-for="(tab, idx) in userTabs" :key="tab.id" :value="idx">
-                <DashboardKpis
-                  :userTabs="userTabs"
-                  :dashboards="dashboards"
-                  :activeTab="activeTab"
-                />
-              </VWindowItem>
-            </VWindow>
-          </v-col>
+          <!-- Tabs for user-configurable dashboards -->
+          <div class="sapling-dashboard-tabs-wrapper d-flex align-center" style="overflow-x: auto; white-space: nowrap; max-width: 100vw;">
+            <VTabs
+              v-model="activeTab"
+              background-color="primary"
+              dark
+              height="44"
+              class="sapling-dashboard-tabs"
+              show-arrows
+              style="min-width: 0; width: 100%; max-width: 100vw;"
+            >
+              <VTab v-for="(tab, idx) in userTabs" :key="tab.id" style="min-width: 120px;">
+                <div class="d-flex align-center sapling-dashboard-tab">
+                  <v-icon class="mr-1" v-if="tab.icon">{{ tab.icon }}</v-icon>
+                  <span class="mr-2">{{ tab.title }}</span>
+                  <v-btn-group>
+                    <v-btn icon="mdi-close" size="x-small" class="transparent" @click.stop="removeTab(idx)" v-if="userTabs.length > 1"/>
+                  </v-btn-group>
+                </div>
+              </VTab>
+              <VTab @click.stop="openDashboardDialog" class="d-flex align-center sapling-dashboard-tab-add" style="min-width: 80px;">
+                <v-icon>mdi-plus</v-icon>
+              </VTab>
+              <!-- Dashboard Anlage Dialog -->
+              <SaplingEdit
+                v-model="dashboardDialog"
+                :mode="'create'"
+                :item="null"
+                :templates="dashboardTemplates || []"
+                :entity="dashboardEntity"
+                @save="onDashboardSave"
+                @cancel="dashboardDialog = false"
+              />
+            </VTabs>
+            <v-btn-group>
+              <v-btn
+                icon="mdi-star-outline"
+                class="transparent"
+                @click="favoritesDrawer = !favoritesDrawer"
+                :title="$t('navigation.favorite')">
+              </v-btn>
+            </v-btn-group>
+          </div>
+        <VWindow v-model="activeTab">
+          <VWindowItem v-for="(tab, idx) in userTabs" :key="tab.id" :value="idx">
+            <DashboardKpis
+              :userTabs="userTabs"
+              :dashboards="dashboards"
+              :activeTab="activeTab"/>
+          </VWindowItem>
+        </VWindow>
 
-          <v-col cols="12" md="2" class="sapling-sideboard d-flex flex-column" style="min-width: 0;">
-            <DashboardFavorites />
-          </v-col>
-        </v-row>
         <SaplingDelete
           v-model:modelValue="dashboardDeleteDialog"
           :item="dashboardToDelete"
           @confirm="confirmDashboardDelete"
           @cancel="cancelDashboardDelete"/>
+        <DashboardFavorites
+          v-model:drawer="favoritesDrawer"
+          :onUpdate:drawer="val => favoritesDrawer = val"
+        />
       </v-container>
     </template>
 </template>
@@ -79,6 +82,7 @@ import SaplingEdit from '@/components/dialog/SaplingEdit.vue';
 // #endregion
 
 // #region Composable
+import { ref } from 'vue';
 const {
   dashboardDeleteDialog,
   dashboardToDelete,
@@ -97,5 +101,6 @@ const {
   removeTab,
 } = useSaplingDashboard();
 
+const favoritesDrawer = ref(false);
 // #endregion
 </script>
--- frontend/src/components/dashboard/SaplingFavorites.vue
+++ frontend/src/components/dashboard/SaplingFavorites.vue
@@ -1,66 +1,78 @@
 <template>
-  <v-card class="sapling-sideboard-card glass-panel" flat>
-    <v-card-title class="text-white">
-      <v-icon left>{{ entity?.icon }}</v-icon> {{ $t('navigation.favorite') }}
-    </v-card-title>
-    <v-divider></v-divider>
+  <v-navigation-drawer
+    v-model="drawer"
+    location="right"
+    app
+    width="340"
+    class="sapling-favorites-drawer glass-panel"
+    temporary
+    style="z-index: 1200; box-shadow: -2px 0 12px rgba(0,0,0,0.15);">
+    <v-card flat class="fill-height glass-panel" style="background: transparent; box-shadow: none;">
+      <v-card-title class="text-white d-flex align-center justify-space-between">
+        <v-icon left>{{ entity?.icon }}</v-icon> {{ $t('navigation.favorite') }}
+        <v-btn-group>
+          <v-btn icon="mdi-close" class="transparent ml-2" size="small" @click="drawer = false"/>
+        </v-btn-group>
+      </v-card-title>
+      <v-divider></v-divider>
 
-    <div class="sapling-sideboard-list-scroll">
-      <v-list dense class="transparent">
-        <v-list-item
-          v-for="(fav, idx) in favorites"
-          :key="fav.handle"
-          @click="goToFavorite(fav)">
-          <div class="d-flex align-center justify-space-between w-100">
-            <div class="d-flex align-center">
-              <v-icon class="mr-2">{{ typeof fav.entity === 'object' && fav.entity?.icon ? fav.entity.icon : 'mdi-bookmark' }}</v-icon>
-              <span class="ml-1">{{ fav.title }}</span>
+      <div class="sapling-sideboard-list-scroll">
+        <v-list dense class="transparent">
+          <v-list-item
+            v-for="(fav, idx) in favorites"
+            :key="fav.handle"
+            @click="goToFavorite(fav)">
+            <div class="d-flex align-center justify-space-between w-100">
+              <div class="d-flex align-center">
+                <v-icon class="mr-2">{{ typeof fav.entity === 'object' && fav.entity?.icon ? fav.entity.icon : 'mdi-bookmark' }}</v-icon>
+                <span class="ml-1">{{ fav.title }}</span>
+              </div>
+              <v-btn icon="mdi-delete" size="x-small" class="glass-panel" @click.stop="removeFavorite(idx)"/>
             </div>
-            <v-btn icon="mdi-delete" size="x-small" class="glass-panel" @click.stop="removeFavorite(idx)"/>
-          </div>
-        </v-list-item>
-      </v-list>
-    </div>
-    <v-divider></v-divider>
+          </v-list-item>
+        </v-list>
+      </div>
+      <v-divider></v-divider>
 
-    <div class="d-flex align-end w-100">
-      <v-btn block color="primary" variant="text" class="d-flex align-center justify-center" @click="openAddFavoriteDialog">
-        <v-icon left>mdi-plus-circle</v-icon>
-        <span>{{ $t('global.add') }}</span>
-      </v-btn>
-    </div>
+      <div class="d-flex align-end w-100">
+        <v-btn block color="primary" variant="text" class="d-flex align-center justify-center" @click="openAddFavoriteDialog">
+          <v-icon left>mdi-plus-circle</v-icon>
+          <span>{{ $t('global.add') }}</span>
+        </v-btn>
+      </div>
 
-    <!-- Add Favorite Dialog -->
-    <v-dialog v-model="addFavoriteDialog" max-width="500" class="sapling-add-favorite-dialog">
-      <v-card class="glass-panel">
-        <v-card-title>{{ $t('global.add') }}</v-card-title>
-        <v-card-text>
-          <v-form ref="favoriteFormRef">
-            <v-text-field
-              v-model="newFavoriteTitle"
-              :label="$t('favorite.title') + '*'"
-              :rules="[v => !!v || $t('favorite.title') + ' ' + $t('global.isRequired')]"
-              required
-            />
-            <v-select
-              v-model="selectedFavoriteEntity"
-              :menu-props="{ contentClass: 'glass-menu'}"
-              :items="entityOptions"
-              :label="$t('navigation.entity') + '*'"
-              return-object
-              :rules="[v => !!v || $t('navigation.entity') + ' ' + $t('global.isRequired')]"
-              required
-            />
-          </v-form>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn text @click="addFavoriteDialog = false">{{ $t('global.cancel') }}</v-btn>
-          <v-btn color="primary" @click="validateAndAddFavorite">{{ $t('global.add') }}</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-card>
+      <!-- Add Favorite Dialog -->
+      <v-dialog v-model="addFavoriteDialog" max-width="500" class="sapling-add-favorite-dialog">
+        <v-card class="glass-panel">
+          <v-card-title>{{ $t('global.add') }}</v-card-title>
+          <v-card-text>
+            <v-form ref="favoriteFormRef">
+              <v-text-field
+                v-model="newFavoriteTitle"
+                :label="$t('favorite.title') + '*'"
+                :rules="[v => !!v || $t('favorite.title') + ' ' + $t('global.isRequired')]"
+                required
+              />
+              <v-select
+                v-model="selectedFavoriteEntity"
+                :menu-props="{ contentClass: 'glass-menu'}"
+                :items="entityOptions"
+                :label="$t('navigation.entity') + '*'"
+                return-object
+                :rules="[v => !!v || $t('navigation.entity') + ' ' + $t('global.isRequired')]"
+                required
+              />
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text @click="addFavoriteDialog = false">{{ $t('global.cancel') }}</v-btn>
+            <v-btn color="primary" @click="validateAndAddFavorite">{{ $t('global.add') }}</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-card>
+  </v-navigation-drawer>
 </template>
 
 <script setup lang="ts">
@@ -69,7 +81,15 @@ import { useSaplingFavorites } from '@/composables/dashboard/useSaplingFavorites
 import '@/assets/styles/SaplingFavorites.css';
 //#endregion
 
-//#region Composable
+
+import { ref, watch } from 'vue';
+// v-model:drawer support
+const props = defineProps<{ drawer: boolean }>();
+const emit = defineEmits(['update:drawer']);
+const drawer = ref(props.drawer);
+watch(() => props.drawer, val => { drawer.value = val; });
+watch(drawer, val => { emit('update:drawer', val); });
+
 const {
   entity,
   favorites,
