7a58e404d400ee75bed0d1dc2946d42cc6de3904
martin-rosbund <martin.rosbund@gmail.com>
2026-03-08 13:40:46 +0100

    mobile view optimized

--- frontend/src/components/table/SaplingTableMultiSelect.vue
+++ frontend/src/components/table/SaplingTableMultiSelect.vue
@@ -2,18 +2,43 @@
   <div v-if="multiSelect" class="multi-select-bar transparent pa-3 mb-2">
     <v-icon color="primary" class="mr-2">mdi-checkbox-multiple-marked</v-icon>
     <span>{{ selectedRows.length }} {{ $t('global.selected') }}</span>
-    <v-btn v-if="selectedRows.length" size="small" color="primary" variant="text" @click="clearSelection">
-      <v-icon start>mdi-close</v-icon>
-      {{ $t('global.clearSelection') }}
-    </v-btn>
-    <v-btn v-if="showActions" size="small" color="primary" variant="text" @click="selectAll">
-      <v-icon start>mdi-select-all</v-icon>
-      {{ $t('global.selectAll') }}
-    </v-btn>
-    <v-btn v-if="selectedRows.length && showActions" size="small" color="error" variant="text" @click="deleteAllSelected">
-      <v-icon start>mdi-delete</v-icon>
-      {{ $t('global.deleteAll') }}
-    </v-btn>
+    <!-- Desktop: Aktionen inline -->
+    <template v-if="showActionsInline">
+      <v-btn v-if="selectedRows.length" size="small" color="primary" variant="text" @click="clearSelection">
+        <v-icon start>mdi-close</v-icon>
+        {{ $t('global.clearSelection') }}
+      </v-btn>
+      <v-btn v-if="showActions" size="small" color="primary" variant="text" @click="selectAll">
+        <v-icon start>mdi-select-all</v-icon>
+        {{ $t('global.selectAll') }}
+      </v-btn>
+      <v-btn v-if="selectedRows.length && showActions" size="small" color="error" variant="text" @click="deleteAllSelected">
+        <v-icon start>mdi-delete</v-icon>
+        {{ $t('global.deleteAll') }}
+      </v-btn>
+    </template>
+    <!-- Mobile: Aktionen im Menü -->
+    <template v-else>
+      <v-menu location="top right" offset-y>
+        <template #activator="{ props }">
+          <v-btn v-bind="props" icon="mdi-dots-vertical" variant="text" size="small" />
+        </template>
+        <v-list class="glass-panel">
+          <v-list-item v-if="selectedRows.length" @click="clearSelection">
+            <v-icon start>mdi-close</v-icon>
+            <span>{{ $t('global.clearSelection') }}</span>
+          </v-list-item>
+          <v-list-item v-if="showActions" @click="selectAll">
+            <v-icon start>mdi-select-all</v-icon>
+            <span>{{ $t('global.selectAll') }}</span>
+          </v-list-item>
+          <v-list-item v-if="selectedRows.length && showActions" @click="deleteAllSelected">
+            <v-icon start>mdi-delete</v-icon>
+            <span>{{ $t('global.deleteAll') }}</span>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </template>
   </div>
 </template>
 
@@ -21,11 +46,23 @@
 import { useI18n } from 'vue-i18n';
 const { t } = useI18n();
 
+import { ref, onMounted } from 'vue';
+import { SaplingWindowWatcher } from '@/utils/saplingWindowWatcher';
 const props = defineProps<{
   multiSelect: boolean;
   selectedRows: number[];
   showActions: boolean;
 }>();
+
+// Responsive: showActionsInline wie im Footer
+const showActionsInline = ref(true);
+let windowWatcher: SaplingWindowWatcher;
+onMounted(() => {
+  windowWatcher = new SaplingWindowWatcher();
+  windowWatcher.onChange((size) => {
+    showActionsInline.value = size !== 'small';
+  });
+});
 
 const emit = defineEmits(['clearSelection', 'deleteAllSelected', 'selectAll']);
 
@@ -40,13 +77,4 @@ function deleteAllSelected() {
 function selectAll() {
   emit('selectAll');
 }
-
 </script>
-
-<style scoped>
-.multi-select-bar {
-  display: flex;
-  align-self: center;
-  height: 40px;
-}
-</style>
