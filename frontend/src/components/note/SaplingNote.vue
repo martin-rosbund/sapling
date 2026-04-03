98f9804821806f11cecab74f5b03881dc9141a2a
martin-rosbund <martin.rosbund@gmail.com>
2026-04-03 21:44:02 +0200

    refactor note

--- frontend/src/components/note/SaplingNote.vue
+++ frontend/src/components/note/SaplingNote.vue
@@ -4,57 +4,97 @@
     class="mx-auto fill-height"
     elevation="12"
     width="100%"
-    type="article, actions, card"/>
+    type="article, actions, card"
+  />
   <template v-else>
     <v-container class="sapling-scrollable" fluid>
       <v-row class="fill-height" density="compact">
         <v-col cols="12" class="d-flex flex-column">
-          <v-tabs v-model="selectedTab" grow background-color="primary" dark height="44">
-            <v-tab v-for="(group, idx) in groups" :key="group.handle ?? idx" :value="idx" >
-              <v-icon class="pr-4" v-if="group.icon" left>{{ group.icon }}</v-icon>
+          <v-tabs
+            v-model="selectedTab"
+            grow
+            background-color="primary"
+            dark
+            height="44"
+          >
+            <v-tab
+              v-for="(group, groupIndex) in groups"
+              :key="String(group.handle ?? groupIndex)"
+              :value="groupIndex"
+            >
+              <v-icon v-if="group.icon" class="pr-4" left>{{ group.icon }}</v-icon>
               {{ $t(`noteGroup.${group.handle}`) }}
             </v-tab>
           </v-tabs>
+
           <v-window v-model="selectedTab" class="flex-grow-1">
             <v-window-item
-              v-for="(group, idx) in groups"
-              :key="group.handle ?? idx"
-              :value="idx"
+              v-for="(group, groupIndex) in groups"
+              :key="String(group.handle ?? groupIndex)"
+              :value="groupIndex"
             >
               <v-row class="pa-2" density="compact">
-                <!-- Add Note as Card -->
                 <v-col
                   v-for="note in currentNotes"
-                  :key="note.handle ?? note.title"
-                  cols="12" sm="6" md="4" lg="3">
-                  <v-card class="sapling-note-card glass-panel tilt-content" v-tilt="TILT_DEFAULT_OPTIONS" outlined>
-                      <div style="position: relative; height: 100%;">
-                        <v-card-title class="d-flex justify-space-between align-center">
-                          <span>{{ note.title }}</span>
-                          <v-btn-group density="compact" style="gap: 2px;">
-                            <v-btn v-if="entityPermission?.allowUpdate" @click="openEditDialog(note)" variant="text"  style="min-width: 28px; padding: 0 4px;">
+                  :key="String(note.handle ?? note.title)"
+                  cols="12"
+                  sm="6"
+                  md="4"
+                  lg="3"
+                >
+                  <v-card
+                    class="sapling-note-card glass-panel tilt-content"
+                    v-tilt="TILT_DEFAULT_OPTIONS"
+                    outlined
+                  >
+                    <div class="sapling-note-card__content">
+                      <v-card-title class="d-flex justify-space-between align-center">
+                        <span>{{ note.title }}</span>
+                        <v-btn-group density="compact" class="sapling-note-card__actions">
+                          <v-btn
+                            v-if="entityPermission?.allowUpdate"
+                            class="sapling-note-card__action"
+                            variant="text"
+                            @click="openEditDialog(note)"
+                          >
                               <v-icon size="x-small">mdi-pencil</v-icon>
-                            </v-btn>
-                            <v-btn v-if="entityPermission?.allowDelete" @click="deleteNote(note)" variant="text" style="min-width: 28px; padding: 0 4px;">
+                          </v-btn>
+                          <v-btn
+                            v-if="entityPermission?.allowDelete"
+                            class="sapling-note-card__action"
+                            variant="text"
+                            @click="openDeleteDialog(note)"
+                          >
                               <v-icon size="x-small">mdi-delete</v-icon>
-                            </v-btn>
-                          </v-btn-group>
-                        </v-card-title>
-                        <v-card-text>
-                          <div class="sapling-note-description">{{ note.description }}</div>
-                        </v-card-text>
-                        <v-card-subtitle class="text-caption text-right" style="position: absolute; right: 0px; bottom: 12px; margin-bottom: 0;">
+                          </v-btn>
+                        </v-btn-group>
+                      </v-card-title>
+                      <v-card-text>
+                        <div class="sapling-note-description">{{ note.description }}</div>
+                      </v-card-text>
+                      <v-card-subtitle class="sapling-note-card__timestamp text-caption text-right">
                           {{ note.createdAt ? $d(new Date(note.createdAt)) : '' }}
-                        </v-card-subtitle>
-                      </div>
+                      </v-card-subtitle>
+                    </div>
                   </v-card>
                 </v-col>
-                <!-- Add Note Button as Card -->
-                <v-col v-if="entityPermission?.allowInsert" cols="12" sm="6" md="4" lg="3">
-                  <v-card outlined class="sapling-add-kpi-card d-flex align-center justify-center glass-panel tilt-content" v-tilt="TILT_DEFAULT_OPTIONS" @click="openCreateDialog">
+
+                <v-col
+                  v-if="entityPermission?.allowInsert && currentGroup"
+                  cols="12"
+                  sm="6"
+                  md="4"
+                  lg="3"
+                >
+                  <v-card
+                    outlined
+                    class="sapling-add-note-card d-flex align-center justify-center glass-panel tilt-content"
+                    v-tilt="TILT_DEFAULT_OPTIONS"
+                    @click="openCreateDialog"
+                  >
                     <v-icon size="large" color="primary">mdi-plus-circle</v-icon>
                     <v-btn color="primary" variant="text" class="ma-2">
-                    {{ $t('global.add') }}
+                      {{ $t('global.add') }}
                     </v-btn>
                   </v-card>
                 </v-col>
@@ -66,49 +106,56 @@
       <SaplingDialogEdit
         :model-value="editDialog.visible"
         :mode="editDialog.mode"
-        :item="editDialog.item ? { ...editDialog.item } : null"
+        :item="editDialogItem"
         :templates="entityTemplates"
         :entity="entity"
         :showReference="false"
-        @update:model-value="val => editDialog.visible = val"
+        @update:model-value="updateEditDialogVisibility"
         @save="saveNoteDialog"
-        @cancel="closeEditDialog" />
+        @cancel="closeEditDialog"
+      />
       <SaplingDialogDelete
         :model-value="deleteDialog.visible"
-        :item="deleteDialog.item ? { ...deleteDialog.item } : null"
-        @update:model-value="val => deleteDialog.visible = val"
+        :item="deleteDialogItem"
+        @update:model-value="updateDeleteDialogVisibility"
         @confirm="confirmDeleteNote"
-        @cancel="closeDeleteDialog" />
+        @cancel="closeDeleteDialog"
+      />
     </v-container>
   </template>
 </template>
 
 <script lang="ts" setup>
 // #region Imports
-import SaplingDialogEdit from '@/components/dialog/SaplingDialogEdit.vue'; // Edit dialog component
-import SaplingDialogDelete from '@/components/dialog/SaplingDialogDelete.vue'; // Delete dialog component
+import SaplingDialogEdit from '@/components/dialog/SaplingDialogEdit.vue';
+import SaplingDialogDelete from '@/components/dialog/SaplingDialogDelete.vue';
 import { TILT_DEFAULT_OPTIONS } from '@/constants/tilt.constants';
 import { useSaplingNote } from '@/composables/note/useSaplingNote';
 // #endregion
 
 // #region Composable
 const {
-    groups,
-    selectedTab,
-    currentNotes,
-    editDialog,
-    deleteDialog,
-    entityTemplates,
-    isLoading,
-    entity,
-    entityPermission,
-    openCreateDialog,
-    openEditDialog,
-    closeEditDialog,
-    saveNoteDialog,
-    deleteNote,
-    closeDeleteDialog,
-    confirmDeleteNote,
+  groups,
+  selectedTab,
+  currentGroup,
+  currentNotes,
+  editDialog,
+  editDialogItem,
+  deleteDialog,
+  deleteDialogItem,
+  entityTemplates,
+  isLoading,
+  entity,
+  entityPermission,
+  openCreateDialog,
+  openEditDialog,
+  updateEditDialogVisibility,
+  closeEditDialog,
+  saveNoteDialog,
+  openDeleteDialog,
+  updateDeleteDialogVisibility,
+  closeDeleteDialog,
+  confirmDeleteNote,
 } = useSaplingNote();
 // #endregion
 </script>
